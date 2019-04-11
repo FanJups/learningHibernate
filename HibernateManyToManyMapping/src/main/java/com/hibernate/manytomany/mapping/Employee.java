1c383866e757b299bf1831b3b1f65c21a2c5101b
FanJups <jupsfan@gmail.com>
2019-04-12 00:57:51 +0200

    Hibernate Many To Many Mapping

---
+++ HibernateManyToManyMapping/src/main/java/com/hibernate/manytomany/mapping/Employee.java
@@ -0,0 +1,60 @@
+package com.hibernate.manytomany.mapping;
+
+import java.util.Set;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	private Set certificates;
+
+	public Employee() {
+	}
+
+	public Employee(String fname, String lname, int salary) {
+		this.firstName = fname;
+		this.lastName = lname;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Set getCertificates() {
+		return certificates;
+	}
+
+	public void setCertificates(Set certificates) {
+		this.certificates = certificates;
+	}
+}
