edc6547332189a0278ccf8fa192070b8a136fa77
FanJups <jupsfan@gmail.com>
2019-04-12 01:27:27 +0200

    Hibernate Component Mapping

---
+++ HibernateComponentMapping/src/main/java/com/hibernate/component/mapping/Employee.java
@@ -0,0 +1,59 @@
+package com.hibernate.component.mapping;
+
+public class Employee implements java.io.Serializable {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	private Address address;
+
+	public Employee() {
+	}
+
+	public Employee(String fname, String lname, int salary, Address address) {
+		this.firstName = fname;
+		this.lastName = lname;
+		this.salary = salary;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
