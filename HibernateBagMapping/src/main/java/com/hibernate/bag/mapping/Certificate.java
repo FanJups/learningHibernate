03e079abf9bb732dd0d9cd90c2a8b40ab35edd5b
FanJups <jupsfan@gmail.com>
2019-04-11 21:38:28 +0200

    Hibernate Bag Mapping

---
+++ HibernateBagMapping/src/main/java/com/hibernate/bag/mapping/Certificate.java
@@ -0,0 +1,31 @@
+package com.hibernate.bag.mapping;
+
+public class Certificate {
+
+	private int id;
+	private String name;
+
+	public Certificate() {
+	}
+
+	public Certificate(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ HibernateBagMapping/src/main/java/com/hibernate/bag/mapping/Employee.java
@@ -0,0 +1,60 @@
+package com.hibernate.bag.mapping;
+
+import java.util.Collection;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	private Collection certificates;
+
+	public Employee() {
+	}
+
+	public Employee(String fname, String lname, int salary) {
+		this.firstName = fname;
+		this.lastName = lname;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Collection getCertificates() {
+		return certificates;
+	}
+
+	public void setCertificates(Collection certificates) {
+		this.certificates = certificates;
+	}
+}
